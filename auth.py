27a4e401539f4a2a2e1a253ef5cc73d795579ea0
Vicente <167089134+Chubiiii@users.noreply.github.com>
2024-11-12 15:57:31 -0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,38 +1,42 @@
+import streamlit as st
+import streamlit_authenticator as stauth
 import yaml
 from yaml.loader import SafeLoader
 
-with open('../config.yaml') as file:
+# Cargar la configuración desde 'config.yaml'
+with open('config.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
 
-# Pre-hashing all plain text passwords once
-# stauth.Hasher.hash_passwords(config['credentials'])
-
+# Configurar el autenticador
 authenticator = stauth.Authenticate(
-    config['credentials'],
-    config['cookie']['name'],
-    config['cookie']['key'],
-    config['cookie']['expiry_days']
+    credentials=config['credentials'],
+    cookie_name=config['cookie']['name'],
+    key=config['cookie']['key'],
+    cookie_expiry_days=config['cookie']['expiry_days'],
+    preauthorized=config['preauthorized']
 )
 
-try:
-    authenticator.login()
-except Exception as e:
-    st.error(e)
-    
-if st.session_state['authentication_status']:
-    authenticator.logout()
-    st.write(f'Welcome *{st.session_state["name"]}*')
-    st.title('Some content')
-elif st.session_state['authentication_status'] is False:
-    st.error('Username/password is incorrect')
-elif st.session_state['authentication_status'] is None:
-    st.warning('Please enter your username and password')
+# Renderizar el formulario de inicio de sesión
+nombre, estado_autenticacion, nombre_usuario = authenticator.login('Iniciar sesión', 'main')
 
-try:
-    email_of_registered_user, \
-    username_of_registered_user, \
-    name_of_registered_user = authenticator.register_user(pre_authorized=config['pre-authorized']['emails'])
-    if email_of_registered_user:
-        st.success('User registered successfully')
-except Exception as e:
-    st.error(e)
+if estado_autenticacion:
+    # Mostrar botón de logout
+    authenticator.logout('Cerrar sesión', 'sidebar')
+    
+    st.sidebar.write(f'Bienvenido/a *{nombre}*')
+    st.title('Contenido de la aplicación')
+    # Aquí puedes colocar el contenido principal de tu aplicación
+elif estado_autenticacion == False:
+    st.error('Nombre de usuario o contraseña incorrectos')
+elif estado_autenticacion == None:
+    st.warning('Por favor ingresa tu nombre de usuario y contraseña')
+
+# Permitir registro de nuevos usuarios si no están autenticados
+if estado_autenticacion != True:
+    try:
+        # Formulario de registro de usuario
+        if authenticator.register_user('Regístrate', preauthorization=True):
+            st.success('Usuario registrado exitosamente')
+            st.info('Por favor, inicia sesión con tus credenciales')
+    except Exception as e:
+        st.error(e)
