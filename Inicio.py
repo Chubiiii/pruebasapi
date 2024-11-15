ea246bd7deba3545b69b6a6b92f18a3e89b63bcf
Vicente <167089134+Chubiiii@users.noreply.github.com>
2024-11-15 10:34:42 -0300

    Create Inicio.py

---
+++ Inicio.py
@@ -0,0 +1,23 @@
+# Home.py
+import streamlit as st
+from PIL import Image
+
+st.set_page_config(page_title="Inicio", page_icon="🏠", layout="centered")
+
+# Agregar una imagen
+image = Image.open("ruta/a/tu/imagen_de_bienvenida.png")
+st.image(image, use_column_width=True)
+
+# Título de bienvenida
+st.title("¡Bienvenido a Nuestra Aplicación!")
+
+# Descripción o mensaje
+st.write("""
+    Esta es la página de inicio de tu aplicación. Haz clic en el botón de abajo para iniciar sesión y acceder a todas las funcionalidades.
+""")
+
+# Botón de bienvenida
+if st.button("Iniciar Sesión"):
+    js = "window.location.href = '/Login'"
+    html = f"<script>{js}</script>"
+    st.markdown(html, unsafe_allow_html=True)
