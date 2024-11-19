2a45b928b2e7898e7712a83df4157b321bd7e8ff
Vicente <167089134+Chubiiii@users.noreply.github.com>
2024-11-19 14:06:24 -0300

    Add files via upload

---
+++ pages/auth.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
+
+
+# Cargar la configuración
+with open('config.yaml') as file:
+    config = yaml.safe_load(file)
+
+# Configurar el autenticador
+authenticator = stauth.Authenticate(
+    credentials=config['credentials'],
+    cookie_name=config['cookie']['name'],
+    cookie_key=config['cookie']['key'],
+    cookie_expiry_days=config['cookie']['expiry_days'],
+    preauthorized=config.get('preauthorized')
+)
+
+# Renderizar el formulario de inicio de sesión
+nombre, estado_autenticacion, nombre_usuario = authenticator.login(button_text='Iniciar sesión',location='main')
+
+if estado_autenticacion:
+    authenticator.logout('Cerrar sesión', 'sidebar')
+    st.sidebar.write(f'Bienvenido/a *{nombre}*')
+    st.title('Contenido de la aplicación')
+    # Aquí coloca el contenido principal de tu aplicación
+elif estado_autenticacion == False:
+    st.error('Nombre de usuario o contraseña incorrectos')
+elif estado_autenticacion == None:
+    st.warning('Por favor ingresa tu nombre de usuario y contraseña')
+
+# Registro de nuevos usuarios
+if estado_autenticacion != True:
+    try:
+        if authenticator.register_user('Regístrate', preauthorization=False):
+            st.success('Usuario registrado exitosamente')
+            st.info('Por favor, inicia sesión con tus credenciales')
+    except Exception as e:
+        st.error(e)
